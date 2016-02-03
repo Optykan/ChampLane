14d7b9db4c28a4adbe1a59ea7b62d37142c88fc6
Optykan <syang0525@gmail.com>
2016-02-03 18:19:24 -0500

    Add ability select in modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-var snodes = document.getElementsByTagName("splash");
-var cnodes = document.getElementsByClassName("champ");
-var splash = document.getElementsByTagName("splash");
-for (var i = 0; i < snodes.length; i++) {
-    snodes[i].style.backgroundImage = "url('assets/splash/center/" + snodes[i].dataset.splash + ".jpg')";
-    cnodes[i].src = "assets/splash/square/" + snodes[i].dataset.splash + ".png";
-    splash[i].onclick = function() {
-        openModal(this);
-    };
-}
-
 function openModal(cName) {
     document.getElementById("mbg").style.backgroundImage = "url('assets/splash/" + cName.dataset.splash + ".jpg')";
     document.getElementById("champ-name").innerHTML = cName.dataset.name;
@@ -23,4 +12,25 @@ function openModal(cName) {
 
 function closeModal() {
     document.getElementById("dim").style.top = "100%";
+}
+
+function makeActive(elem) {
+    var active = document.getElementsByClassName("active")[0];
+    active.className = "ability";
+    elem.className = "ability active";
+}
+var cnodes = document.getElementsByClassName("champ");
+var snodes = document.getElementsByTagName("splash");
+for (var a = 0; a < 5; a++) {
+    document.getElementsByClassName("ability")[a].onclick = function() {
+        makeActive(this);
+    }
+}
+
+for (var i = 0; i < snodes.length; i++) {
+    snodes[i].style.backgroundImage = "url('assets/splash/center/" + snodes[i].dataset.splash + ".jpg')";
+    cnodes[i].src = "assets/splash/square/" + snodes[i].dataset.splash + ".png";
+    snodes[i].onclick = function() {
+        openModal(this);
+    }
 }
